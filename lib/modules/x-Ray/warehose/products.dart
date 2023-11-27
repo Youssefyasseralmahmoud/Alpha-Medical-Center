5decad878be1ddfb76e6b6bab33e8df340c5af66
Youssefyasseralmahmoud <133138102+Youssefyasseralmahmoud@users.noreply.github.com>
2023-11-27 18:36:34 -0500

    Delete lib/modules/x-Ray directory

--- lib/modules/x-Ray/warehose/products.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-class Product {
-  final String name;
-  int quantity;
-
-  Product({required this.name, this.quantity = 0});
-}

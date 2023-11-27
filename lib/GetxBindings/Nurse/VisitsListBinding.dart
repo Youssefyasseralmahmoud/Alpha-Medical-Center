dd2f7309b867f981149bbb24725698e705b1deed
Youssefyasseralmahmoud <133138102+Youssefyasseralmahmoud@users.noreply.github.com>
2023-11-27 18:32:26 -0500

    Delete lib/GetxBindings/Nurse directory

--- lib/GetxBindings/Nurse/VisitsListBinding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:project_after_update/modules/Nurse/VisitsList/VisitsListController.dart';
-class VisitsListBinding implements Bindings{
-  @override
-  void dependencies() {
-   Get.put<VisitsListController>(VisitsListController())  ;
-   }
-}
